92576de2587ad4e70062f3a79d3d6d0e6df5cb87
MunChan Park <kaki104@hanmail.net>
2019-03-16 18:38:47 +0900

    Prism으로 시작하는 UWP app Part6 - 상세 페이지

---
+++ PrismSample.Core/Models/Rating.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrismSample.Core.Models
+{
+    public class Rating
+    {
+        public string Source { get; set; }
+        public string Value { get; set; }
+    }
+
+}
--- PrismSample/ViewModels/MovieDetailViewModel.cs
+++ PrismSample/ViewModels/MovieDetailViewModel.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using Windows.UI.Xaml.Navigation;
 using Prism.Windows.Mvvm;
 using Prism.Windows.Navigation;
+using PrismSample.Core.Helpers;
+using PrismSample.Core.Models;
 
 namespace PrismSample.ViewModels
 {
     public class MovieDetailViewModel : ViewModelBase
     {
         private readonly INavigationService _navigationService;
+        private string _movieId;
+        private MovieDetail _currentMovieDetail;
+
         public MovieDetailViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
@@ -21,7 +27,34 @@ namespace PrismSample.ViewModels
             if (e.NavigationMode == NavigationMode.New)
             {
                 Debug.WriteLine(e.Parameter);
+                _movieId = e.Parameter as string;
+
+                GetMovieDetail();
             }
+        }
+
+        private async void GetMovieDetail()
+        {
+            if (string.IsNullOrEmpty(_movieId)) return;
+
+            using (var client = new HttpClient())
+            {
+                var url = $"http://www.omdbapi.com/?i={_movieId}&apikey=32941a88";
+                var result = await client.GetStringAsync(url);
+                if (string.IsNullOrEmpty(result)) return;
+
+                var movieDetail = await Json.ToObjectAsync<MovieDetail>(result);
+                if (movieDetail == null) return;
+                CurrentMovieDetail = movieDetail;
+            }
+        }
+        /// <summary>
+        /// 현재 영화 상세
+        /// </summary>
+        public MovieDetail CurrentMovieDetail
+        {
+            get => _currentMovieDetail;
+            set => SetProperty(ref _currentMovieDetail ,value);
         }
     }
 }
